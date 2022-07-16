a52a00b3b551b23a0e1262e32de395e09f296768
Ciptadi Eko Setya Wasono <wong.galaxy@gmail.com>
2022-07-16 18:20:19 +0700

    move db

--- utils/db.js
+++ utils/db.js
@@ -1,5 +1,7 @@
+
 const mongoose = require('mongoose')
-const uri = 'mongodb://127.0.0.1:27017/espj'
+//  const uri = 'mongodb://127.0.0.1:27017/espj'
+const uri = "mongodb+srv://ciptadi:1Jakx13bESVtl1uj@cluster0.92ixh.mongodb.net/?retryWrites=true&w=majority"
 mongoose.connect(uri, {
   useNewUrlParser: true, 
   useUnifiedTopology: true,
@@ -7,3 +9,8 @@ mongoose.connect(uri, {
 })
 .then(() => console.log('MongoDB connected...'))
 .catch(err => console.log(err));
+
+
+
+
+
